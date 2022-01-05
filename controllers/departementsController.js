44847ea601445c9e0482dccb7c6879283ee8ed2d
benkrakar <benkrakarwalid44@gmail.com>
2022-01-05 16:06:33 +0100

    controllers creation

---
+++ controllers/departementsController.js
@@ -0,0 +1,15 @@
+exports.getAllDepartements = (req, res) => {
+  console.log("getAllDepartements");
+};
+exports.createDepartement= (req, res) => {
+  console.log("createdepartement");
+};
+exports.getDepartement= (req, res) => {
+  console.log("getdepartement");
+};
+exports.updateDepartement= (req, res) => {
+  console.log("updatedepartement");
+};
+exports.deleteDepartement= (req, res) => {
+  console.log("deletedepartement");
+};
---
+++ controllers/usersController.js
@@ -0,0 +1,15 @@
+exports.getAllUsers = (req, res) => {
+  console.log("getAllUsers");
+};
+exports.createUser = (req, res) => {
+  console.log("createUser");
+};
+exports.getUser = (req, res) => {
+  console.log("getUser");
+};
+exports.updateUser = (req, res) => {
+  console.log("updateUser");
+};
+exports.deleteUser = (req, res) => {
+  console.log("deleteUser");
+};
